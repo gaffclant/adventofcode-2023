7adce704827dbcf379c7c0824a3bdd6b4727634c
Nic Gaffney <gaffney_nic@protonmail.com>
2023-12-07 17:42:39 -0600

    Day 2 parts 1 and 2 optimized (kinda)

--- day2/C/src/part2.c
+++ day2/C/src/part2.c
@@ -6,7 +6,6 @@
 
 void itergame(char **games, int *mr, int *mg, int *mb) {
     int num = strtol(*games, games, 10);
-    // printf("\titergame:\tgam: %s\tnum: %i\n", *games, num);
     switch ((++*games)[0]) {
     case 'r':
         *mr = num > *mr ? num : *mr;
@@ -22,6 +21,16 @@ void itergame(char **games, int *mr, int *mg, int *mb) {
     }
 }
 
+void outeritergame(char **games, int *mr, int *mg, int *mb, char delim) {
+    char *gam = malloc(LINESIZE);
+    gam = strncpy(gam, *games, strchr(*games, delim) - *games);
+    while (strchr(gam, ',')) {
+        itergame(&gam, mr, mg, mb);
+        gam = strchr(gam, ',') + 2;
+    }
+    itergame(&gam, mr, mg, mb);
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         printf("Usage: %s <file>\n", argv[0]);
@@ -33,46 +42,24 @@ int main(int argc, char **argv) {
         printf("File not found: %s\n", argv[1]);
         return 1;
     }
-    printf("File opened\n");
 
     size_t linesize = LINESIZE;
     char *line = malloc(linesize);
-    int id = 0;
     int result = 0;
     while (-1 != getline(&line, &linesize, file)) {
-        printf("%i\n", result);
         line = strchr(line, ' ') + 1;
-        printf("----------\nLine:\t%s", line);
-        char *games = malloc(512);
-        id = strtol(line, &games, 10);
-        games = strchr(games, ':') + 2;
-        // printf("Games:\t%s", games);
+        char *games = malloc(LINESIZE);
+        games = strchr(line, ':') + 2;
         int maxred = 0;
         int maxgreen = 0;
         int maxblue = 0;
         while (strchr(games, ';')) {
-            char *gam = malloc(LINESIZE);
-            gam = strncpy(gam, games, strchr(games, ';') - games);
-            // printf("--\nGam:\t%s\n", gam);
-            while (strchr(gam, ',')) {
-                itergame(&gam, &maxred, &maxgreen, &maxblue);
-                gam = strchr(gam, ',') + 2;
-            }
-            itergame(&gam, &maxred, &maxgreen, &maxblue);
+            outeritergame(&games, &maxred, &maxgreen, &maxblue, ';');
             games = strchr(games, ';') + 2;
         }
-        char *gam = malloc(LINESIZE);
-        gam = strncpy(gam, games, strchr(games, '\n') - games);
-        // printf("--\nGam:\t%s\n", gam);
-        while (strchr(gam, ',')) {
-            itergame(&gam, &maxred, &maxgreen, &maxblue);
-            gam = strchr(gam, ',') + 2;
-        }
-        itergame(&gam, &maxred, &maxgreen, &maxblue);
+        outeritergame(&games, &maxred, &maxgreen, &maxblue, '\n');
 
-        printf("r: %i g: %i b: %i\n", maxred, maxgreen, maxblue);
         result += maxred * maxgreen * maxblue;
-        printf("%i is valid\n", id);
     }
 
     printf("%i\n", result);
